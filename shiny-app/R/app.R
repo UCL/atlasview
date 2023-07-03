fe3c9227e48d1d1554343f093d7b4b5111fe101c
Asif Tamuri <tamuri@gmail.com>
2023-07-03 10:50:33 +0100

    implement circos plots in new UI

--- shiny-app/R/app.R
+++ shiny-app/R/app.R
@@ -1,5 +1,56 @@
 atlasviewApp <- function(...) {
   server <-  function(input, output) {
+    
+    # update that index disease selection drop-down
+    observe({
+      # get the index diseases for the speciality
+      index_diseases <- index_diseases %>% filter(speciality_code == input$select_speciality) %>% select(phecode_index_dis, phenotype_index_dis)
+      
+      # if any were found
+      if (nrow(index_diseases) > 0) {
+        # update the index disease select box
+        updateSelectizeInput(session=getDefaultReactiveDomain(),
+                             inputId = "select_index_disease", 
+                             choices = split(index_diseases$phecode_index_dis, index_diseases$phenotype_index_dis), 
+                             selected = NULL, 
+                             options = list(placeholder = 'Please select an index disease', 
+                                            onInitialize = I('function() { this.setValue(""); }'))
+        )
+      } else {
+        # no index diseases found for the speciality - empty the select box
+        updateSelectizeInput(session=getDefaultReactiveDomain(),
+                             input="select_index_disease",
+                             choices=list(),
+                             selected=NULL,
+                             options=list(placeholder=''))
+        
+      }
+    })
+    
+    output$indexDiseaseName <- renderText({
+      if (!is.null(input$select_index_disease) & input$select_index_disease != "") {
+        return((index_diseases %>% filter(phecode_index_dis == input$select_index_disease) %>% select(phenotype_index_dis))[[1,1]])
+      } else {
+        return('')
+      }
+    })
+    
+    # show circos plot for the chosen disease
+    output$circosPlot <- renderSvgPanZoom({
+      # if an index disease has been selected
+      if (!is.null(input$select_index_disease) & input$select_index_disease != "") {
+        plot_filename = paste0("plot_", input$select_index_disease, ".svg")
+        
+        # if we haven't generated the plot already
+        if (!file.exists(plot_filename)) {
+          make_plot(get_cooccurring_diseases(MM_processed, input$select_index_disease), to_svg=TRUE)
+        }
+        
+        svgPanZoom(read_file(plot_filename), zoomScaleSensitivity=0.5)
+        
+      }
+    })
+    
   }
   
   shinyApp(ui = atlasview_ui, server = server)
--- shiny-app/R/ui.R
+++ shiny-app/R/ui.R
@@ -4,23 +4,28 @@ fpath2 <- system.file("extdata", "lkp_unique_spec_circo_plot_codes.csv", package
 
 specialties <- cbind(read_csv(fpath1), read_csv(fpath2))
 
+# information about index and coocurring diseases
+fpath3 <-  system.file("extdata", "MM_for_circo_network_vis_29112022.csv", package="atlasview")
+
+index_diseases <- read_csv(fpath3) %>% 
+  select(phecode_index_dis, phenotype_index_dis) %>% 
+  distinct() %>% 
+  arrange(phenotype_index_dis) %>%
+  mutate(speciality_code="GAST")  # TODO: this data only has GAST diseases
+
 atlasview_ui <- fluidPage(
   title = "Shiny Application",
   h1(
     "AtlasView"
   ),
-  inputPanel(
+  wellPanel(
     selectizeInput('select_speciality', 
                    'Speciality', 
                    choices = split(specialties$code, specialties$speciality), 
                    options = list(placeholder = 'Please select a speciality', 
                                   onInitialize = I('function() { this.setValue(""); }'))
     ), 
-    selectInput(
-      inputId = "input_unhzahtfhg",
-      label = "Index disease",
-      choices = NULL
-    )
+   selectizeInput( 'select_index_disease','Index disease', choices = list(), options = list(placeholder = '') ),
   ),
   tabsetPanel(
     type = "tabs",
@@ -41,8 +46,10 @@ atlasview_ui <- fluidPage(
     tabPanel(
       title = "circos",
       value = "circos",
-      plotOutput(
-        outputId = "output_bxnu7x9xpc"
+      box(width=12,
+          title=textOutput(outputId="indexDiseaseName"),
+          add_busy_spinner(spin = "fading-circle"),
+          svgPanZoomOutput(outputId = "circosPlot", width="1000px", height="1000px")
       )
     )
   ),
