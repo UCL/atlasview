58289293fe8a2d9a75fd83020aaa29da7c9f67a8
Asif Tamuri <tamuri@gmail.com>
2023-07-05 17:15:38 +0100

    tidying up functions

--- shiny-app/R/ui.R
+++ shiny-app/R/ui.R
@@ -11,12 +11,6 @@ jsCode <- "shinyjs.updateRemark = function(params) {
   }
 }"
 
-now_utc <- function() {
-  now <- Sys.time()
-  attr(now, "tzone") <- "UTC"
-  now
-}
-
 id <- stringr::str_remove_all(uuid::UUIDgenerate(), "-")
 
 authentication <- jose::jwt_claim(
--- shiny-app/R/utils.R
+++ shiny-app/R/utils.R
@@ -1,22 +1,30 @@
-#' Get the full path to a AtlasView data file 
-#' @param filename name of a file
-#' @NoRd
-get_data_filepath <- function(filename) {
-  home_res <- Sys.getenv("ATLASVIEW_DATA_PATH")
-  paste(home_res, "/", filename, sep='')
+#' Get the path to data files
+#' NoRd
+get_data_root <- function() {
+  Sys.getenv("ATLAS_DATA_PATH")
 }
 
 #' Get the full path to a AtlasView data file 
 #' @param filename name of a file
 #' @NoRd
+get_data_filepath <- function(filename) {
+  paste(get_data_root(), "/", filename, sep='')
+}
+
+#' Load the list of specialties, with their code
+#' @NoRd
 get_specialties <- function() {
-  
-  home_res <- Sys.getenv("ATLASVIEW_DATA_PATH")
-  
   # list of all specialties
-  fpath1 <- paste0(home_res, "/lkp_unique_spec_circo_plot.csv")
-  fpath2 <- paste0(home_res, "/lkp_unique_spec_circo_plot_codes.csv")
-  
-  specialties <- cbind(readr::read_csv(fpath1, show_col_types = FALSE), readr::read_csv(fpath2, show_col_types = FALSE))
-  specialties
+  cbind(
+    readr::read_csv(get_data_filepath("lkp_unique_spec_circo_plot.csv"), show_col_types = FALSE), 
+    readr::read_csv(get_data_filepath("lkp_unique_spec_circo_plot_codes.csv"), show_col_types = FALSE)
+  )
+}
+
+#' Get the current UTC time
+#' @NoRd
+now_utc <- function() {
+  now <- Sys.time()
+  attr(now, "tzone") <- "UTC"
+  now
 }
