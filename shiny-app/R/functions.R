UCL/atlasview

commit 746c36608884eef6527a7742a0d8ff5da4960f9e

Explicitly set `colClasses` for `fread`
Avoids issues with empty data

shiny-app/R/functions.R
@@ -15,16 +15,19 @@ get_data_filepath <- function(filename) {
 
 #' Load, process, and return data for specialties and diseases
 get_atlasview_data <- function() {
-  specialties <- data.table::fread(get_data_filepath("specialties.csv"), header = TRUE) %>%
+  specialties <- data.table::fread(get_data_filepath("specialties.csv"), header = TRUE,
+                                   colClasses = c("character", "character")) %>%
     dplyr::arrange(code)
   
-  specialty_colours <- data.table::fread(get_data_filepath("lkp_spe_col.csv"), header = TRUE)
+  specialty_colours <- data.table::fread(get_data_filepath("lkp_spe_col.csv"), header = TRUE,
+                                         colClasses = c("character", "character"))
   
   specialties <- specialties %>% dplyr::left_join(y = specialty_colours, by="specialty")   # for circos plots
   specialty_colours <- setNames(as.character(specialty_colours$color), specialty_colours$specialty)  # for caterpillar plots
   
   #read full MM res in vis format
-  MM_res <- data.table::fread(file=get_data_filepath("MM_for_circo_network_vis.csv")) %>% 
+  MM_res <- data.table::fread(file=get_data_filepath("MM_for_circo_network_vis.csv"),
+                              colClasses = list(character = c(1:9, 13), numeric = 10:12)) %>% 
     dplyr::left_join(y = specialties, by=c("specialty_index_dis" = "specialty")) %>%
     dplyr::rename("specialty_code" = "code") %>%
     dplyr::left_join(y = specialties, by=c("specialty_cooccurring_dis" = "specialty")) %>%

commit 15b1ea3d973fa5cd76448aef1807cd7372ee5864

Bug fix: prevalence column (9) should be numeric
Bug introduced after 746c36608884eef6527a7742a0d8ff5da4960f9e

shiny-app/R/functions.R
@@ -27,7 +27,7 @@ get_atlasview_data <- function() {
   
   #read full MM res in vis format
   MM_res <- data.table::fread(file=get_data_filepath("MM_for_circo_network_vis.csv"),
-                              colClasses = list(character = c(1:9, 13), numeric = 10:12)) %>% 
+                              colClasses = list(character = c(1:8, 13), numeric = 9:12)) %>% 
     dplyr::left_join(y = specialties, by=c("specialty_index_dis" = "specialty")) %>%
     dplyr::rename("specialty_code" = "code") %>%
     dplyr::left_join(y = specialties, by=c("specialty_cooccurring_dis" = "specialty")) %>%