5e082a7498d4a63f8943f1c127b3836cbbd39b4c
Milan Malfait <m.malfait@ucl.ac.uk>
2023-09-29 16:52:23 +0100

    Pass command line  arguments to playbook

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ## Source the environment variables and run playbook
-source .env_ansible && ansible-playbook playbook.yml -i ./hosts.yml
+source .env_ansible && ansible-playbook playbook.yml -i ./hosts.yml "$@"
