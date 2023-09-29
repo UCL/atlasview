c796552bc1bf4f36e4b357673689dd73c78da9f3
Milan Malfait <m.malfait@ucl.ac.uk>
2023-09-29 13:52:14 +0100

    GHA: Set up Remark42 during deployment

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,6 +6,13 @@ echo $PWD
 # Show current status of the repo
 git describe --tags
 
+# Set up Remark42 engine
+git submodule update --init --recursive
+cd remark42/remark42
+git apply ../*.patch
+
+cd $HOME/atlasview
+
 # Check that docker is installed and build images
 sudo docker --version
 sudo docker compose up -d --build
