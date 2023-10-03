da00289dc0ff7564645d14418d1259367a3028e7
Milan Malfait <m.malfait@ucl.ac.uk>
2023-10-03 16:53:50 +0100

    caterpillar and helpers: Get rid of pipes + formatting

--- shiny-app/R/functions.R
+++ shiny-app/R/functions.R
@@ -16,32 +16,38 @@ get_data_filepath <- function(filename) {
 #' Load, process, and return data for specialties and diseases
 #' @importFrom stats setNames
 get_atlasview_data <- function() {
-  specialties <- data.table::fread(get_data_filepath("specialties.csv"), header = TRUE,
-                                   colClasses = c("character", "character")) %>%
-    dplyr::arrange(code)
+  specialties <- data.table::fread(
+    get_data_filepath("specialties.csv"),
+    header = TRUE, colClasses = c("character", "character")
+  )
+  specialties <- dplyr::arrange(specialties, code)
   
-  specialty_colours <- data.table::fread(get_data_filepath("lkp_spe_col.csv"), header = TRUE,
-                                         colClasses = c("character", "character"))
+  specialty_colours <- data.table::fread(get_data_filepath("lkp_spe_col.csv"),
+    header = TRUE, colClasses = c("character", "character")
+  )
   
-  specialties <- specialties %>% dplyr::left_join(y = specialty_colours, by="specialty")   # for circos plots
+  specialties <- dplyr::left_join(specialties, specialty_colours, by = "specialty") # for circos plots
   specialty_colours <- setNames(as.character(specialty_colours$color), specialty_colours$specialty)  # for caterpillar plots
   
-  #read full MM res in vis format
-  MM_res <- data.table::fread(file=get_data_filepath("MM_for_circo_network_vis.csv"),
-                              colClasses = list(character = c(1:8, 13), numeric = 9:12)) %>% 
-    dplyr::left_join(y = specialties, by=c("specialty_index_dis" = "specialty")) %>%
-    dplyr::rename("specialty_code" = "code") %>%
-    dplyr::left_join(y = specialties, by=c("specialty_cooccurring_dis" = "specialty")) %>%
-    dplyr::rename("cooccurring_specialty_code" = "code")
+  # read full MM res in vis format
+  MM_res <- data.table::fread(
+    file = get_data_filepath("MM_for_circo_network_vis.csv"),
+    colClasses = list(character = c(1:8, 13), numeric = 9:12)
+  )
+  
+  MM_res <- dplyr::left_join(MM_res, specialties, by = c("specialty_index_dis" = "specialty"))
+  MM_res <- dplyr::rename(MM_res, "specialty_code" = "code")
+  MM_res <- dplyr::left_join(MM_res, specialties, by = c("specialty_cooccurring_dis" = "specialty"))
+  MM_res <- dplyr::rename(MM_res, "cooccurring_specialty_code" = "code")
   
   # N of diseases and specialties 
   n_dis_spe <- data.table::fread(file = get_data_filepath("MM_2_n.csv"))
   
   # information about index and co-occurring diseases
-  index_diseases <- MM_res %>% 
-    dplyr::select(phecode_index_dis, phenotype_index_dis, specialty_index_dis, specialty_code) %>% 
-    dplyr::distinct() %>% 
-    dplyr::arrange(phenotype_index_dis)
+  index_diseases <- dplyr::select(MM_res,
+    phecode_index_dis, phenotype_index_dis, specialty_index_dis, specialty_code
+  )
+  index_diseases <- dplyr::arrange(dplyr::distinct(index_diseases), phenotype_index_dis)
   
   list(
     specialties = specialties,
