52083202f8e2df8cd78e0ffdad2e067c08627b86
Milan Malfait <m.malfait@ucl.ac.uk>
2023-10-04 16:39:59 +0100

    Add script to update R package's version

---
+++ deployment/update-shiny-app-version.R
@@ -0,0 +1,12 @@
+## Set the version of the Shiny app to the latest git tag
+## ======================================================
+
+usethis::proj_activate("shiny-app")
+
+tags <- gert::git_tag_list()
+version <- sub("^v", "", tags$name[1])
+
+## Update DESCRIPTION file
+desc <- desc::desc()
+desc$set_version(version)
+desc$write()
