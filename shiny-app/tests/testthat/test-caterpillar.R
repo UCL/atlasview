986e20f76ca6e65dd20874b5fb3fb04f29c5b160
Milan Malfait <m.malfait@ucl.ac.uk>
2023-10-13 13:27:11 +0100

    Add test for `get_cooccurring_diseases()`

---
+++ shiny-app/tests/testthat/test-caterpillar.R
@@ -0,0 +1,21 @@
+test_data <- get_atlasview_data()
+test_MM_res <- test_data$MM_res
+select_specialty <- sample(unique(test_MM_res$specialty_code), 1)
+select_index_disease <- sample(get_index_diseases(test_data$index_diseases, select_specialty), 1)
+
+test_caterpillar_data <- get_cooccurring_diseases(test_data$MM_res,
+  index_disease = select_index_disease, specialty = select_specialty
+)
+
+test_that("get_cooccurring_diseases() returns correct format for caterpillar plot", {
+  expect_s3_class(test_caterpillar_data, "data.frame")
+  expect_true(nrow(test_caterpillar_data) > 0)
+  expect_true(ncol(test_caterpillar_data) > 0)
+  
+  ## Need at least these names for caterpillar plot to work
+  expected_names <- c(
+    "prevalence", "specialty_cooccurring_dis", "prev_ratio",
+    "ci_left_prev_ratio", "ci_right_prev_ratio"
+  )
+  expect_contains(names(test_caterpillar_data), expected_names)
+})
