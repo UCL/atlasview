04d10efaabe90f7b7f70258a1c1478a5e2c8a79a
Milan Malfait <m.malfait@ucl.ac.uk>
2023-10-13 15:41:22 +0100

    Add tests for caterpillar plots

--- shiny-app/tests/testthat/test-caterpillar.R
+++ shiny-app/tests/testthat/test-caterpillar.R
@@ -1,7 +1,10 @@
-test_data <- get_atlasview_data()
+test_data <- atlasview_mock_data
 test_MM_res <- test_data$MM_res
-select_specialty <- sample(unique(test_MM_res$specialty_code), 1)
-select_index_disease <- sample(get_index_diseases(test_data$index_diseases, select_specialty), 1)
+
+withr::with_seed(20231013, {
+  select_specialty <- sample(unique(test_MM_res$specialty_code), 1)
+  select_index_disease <- sample(get_index_diseases(test_data$index_diseases, select_specialty), 1)
+})
 
 test_caterpillar_data <- get_cooccurring_diseases(test_data$MM_res,
   index_disease = select_index_disease, specialty = select_specialty
@@ -18,4 +21,14 @@ test_that("get_cooccurring_diseases() returns correct format for caterpillar plo
     "ci_left_prev_ratio", "ci_right_prev_ratio"
   )
   expect_contains(names(test_caterpillar_data), expected_names)
+})
+
+test_that("caterpillar_prevalence_plot() produces consistent plot", {
+  p <- caterpillar_prevalence_plot(test_caterpillar_data)
+  vdiffr::expect_doppelganger("caterpillar_prevalence", p)
+})
+
+test_that("caterpillar_prevalence_ratio_plot() produces consistent plot", {
+  p <- caterpillar_prevalence_ratio_plot(test_caterpillar_data)
+  vdiffr::expect_doppelganger("caterpillar_prevalence_ratio", p)
 })
