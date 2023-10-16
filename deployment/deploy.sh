d245dd25f804b8b8e3a7ac02fcae5a4cf2c9b1ee
Milan Malfait <m.malfait@ucl.ac.uk>
2023-10-16 13:08:58 +0100

    Make sure remark patches are applied

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -12,6 +12,10 @@ echo "\n*** CURRENT VERSION: $(git describe --tags) ***\n"
 echo "Make sure Remark42 patches were applied..."
 cd remark42/remark42
 git apply --reverse --check ../*.patch  # fails if patches were not applied
+if ! $(git apply --check ../*.patch &> /dev/null); then
+  exit 1
+fi
+
 git status                              # should show changes to auth-panel.tsx and profile.tsx
 
 cd $HOME/atlasview
