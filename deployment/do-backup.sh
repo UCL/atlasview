d80103e96be4fc2dbb27fad3ec62219f1cd5b35c
Milan Malfait <m.malfait@ucl.ac.uk>
2023-10-16 15:54:51 +0100

    Small fix to backup script

--- deployment/do-backup.sh
+++ deployment/do-backup.sh
@@ -5,11 +5,9 @@ cd ${HOME}/atlasview-backups/comments
 # connect to the remark container and trigger a backup
 docker exec atlasview-remark-1 backup
 
-# make sure we have access to remark backup files
-sudo chown -R ${USER} ${HOME}/atlasview-data/remark/backup
-
 # run script to convert backup of comments into an excel file
 # use the latest created backup file, whatever the name
+echo "Converting comments backup to Excel file"
 ${HOME}/atlasview/remark42/backup2excel.py "$(ls -dAt ${HOME}/atlasview-data/remark/backup/* | head -n1)"
 
 # create a timestamped backup of atlasview-data except caches and data files etc
