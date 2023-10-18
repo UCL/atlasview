9df916a68312ead97f863e4e11439d9a51a82007
Milan Malfait <m.malfait@ucl.ac.uk>
2023-10-18 11:22:57 +0100

    Write circos plots to temp files during tests
    Avoids creating `Rplots.pdf` files

--- shiny-app/tests/testthat/test-plot_circos.R
+++ shiny-app/tests/testthat/test-plot_circos.R
@@ -14,21 +14,23 @@ test_that("circos_plot() produces consistent output", {
   vdiffr::expect_doppelganger("circos_plot", p)
 })
 
-test_that("circos_plot() resets circos params on exit", {
-  old_params <- circos.par()
-  circos_plot(test_data, select_index_disease, svg_filepath = NULL)
-  new_params <- circos.par()
-  expect_identical(new_params, old_params) 
-})
+## Create temp file to write plot output to, to avoid creating `Rplots.pdf` files
+tmp_file <- withr::local_tempfile(fileext = ".svg")
 
 test_that("circos_plot() produces SVG file when `svg_filepath` defined", {
-  tmp_file <- withr::local_tempfile(fileext = ".svg")
   circos_plot(test_data, select_index_disease, svg_filepath = tmp_file)
   expect_true(file.exists(tmp_file))
 })
 
+test_that("circos_plot() resets circos params on exit", {
+  old_params <- circos.par()
+  circos_plot(test_data, select_index_disease, svg_filepath = tmp_file)
+  new_params <- circos.par()
+  expect_identical(new_params, old_params) 
+})
+
 test_that("circos_plot() does not produce warning messages", {
   expect_no_message({
-    circos_plot(test_data, select_index_disease, svg_filepath = NULL)
+    circos_plot(test_data, select_index_disease, svg_filepath = tmp_file)
   })
 })
