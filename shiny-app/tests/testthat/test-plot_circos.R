712f02c672b83dcb2631f894e6182b9321709f13
Milan Malfait <m.malfait@ucl.ac.uk>
2023-10-19 23:13:48 +0100

    Update random seed for circos plot test
    No idea why, but this was giving discrepencies between tests run on different systems.

--- shiny-app/tests/testthat/test-plot_circos.R
+++ shiny-app/tests/testthat/test-plot_circos.R
@@ -3,7 +3,7 @@ suppressWarnings({
 })
 test_MM_res <- test_data$MM_res
 
-withr::with_seed(20231017, {
+withr::with_seed(20231019, {
   select_specialty <- sample(unique(test_MM_res$specialty_code), 1)
   select_index_disease <- sample(get_index_diseases(test_data$index_diseases, select_specialty), 1)
 })
